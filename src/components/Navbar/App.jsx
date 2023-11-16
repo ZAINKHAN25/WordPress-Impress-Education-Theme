ee3588f269a09cf09342271ccfdee1e4d52bef25
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2023-11-16 21:16:20 +0500

    Navbar Done

---
+++ src/components/Navbar/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App(){
+    return (
+        <nav>
+            <div className="brandName">Learn Zoom</div> 
+            <div className="navbtnarea">
+                <span>Home</span>
+                <span>About</span>
+                <span>Courses</span>
+                <span>Testimonial</span>
+                <span>Contact</span>
+            </div>
+            <div className="buttonAreaNav">
+                <button>Contact Us</button>
+            </div>
+        </nav>
+    )
+}
+
+export default App;
